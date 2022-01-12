546f48dfb91a96e7f864a3cf655edf6e3ade9c33
June <yjp243@naver.com>
2022-01-12 12:03:12 +0900

    study.main 생성

---
+++ Step02_DataType/src/study/main/MainClass01.java
@@ -0,0 +1,18 @@
+package study.main;
+
+public class MainClass01 {
+	public static void main(String[] args) {
+		System.out.println("연습입니다.");
+		
+		//정수
+		byte a=127;
+		short b=32767;
+		int c=2147483647;
+		long d=123456789;
+		
+		//실수
+		float e=10.1f;
+		double f=10.1d;
+		double g=10.1;
+	}
+}
---
+++ Step02_DataType/src/study/main/MainClass02.java
@@ -0,0 +1,19 @@
+package study.main;
+
+public class MainClass02 {
+	public static void main(String[] args) {
+		System.out.println("연습중입니다");
+		
+		//변수 선언과 대입값 정하기
+		int num1=1;
+		//변수만 선언하기
+		int	num2;
+		//대입값을 정하기
+		num2=10;
+		
+		//정수는 byte < short < int < long 순서로 표현 가능범위가 늘어난다
+		
+		int myint=100;
+		byte mybyte=100;
+	}
+}
